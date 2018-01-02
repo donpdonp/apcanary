eeb337f103cb7535c767a5aa2d0554d4affc64ed
Don Park <donp@donp.org>
2018-01-02 11:46:46 +0700

    WebRequest

--- vala/lua/capture.lua
+++ vala/lua/capture.lua
@@ -1,4 +1,6 @@
 http = require("https")
+ltn12 = require("ltn12")
+
 http.TIMEOUT=10
 
 function capture(url)
@@ -6,22 +8,34 @@ function capture(url)
   if string.match(url, "https://wifi.ais.co.th") then
     local urlr = "https://wifi.ais.co.th/" --fix url
     print ("lua loading real " .. urlr)
+    local t = {}
     local b, c, h, sl = http.request {
         url = urlr,
-        headers = { ["User-Agent"]= "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0",
-                    ["Accept"] = "*/*",
-                    ["Host"] = "wifi.ais.co.th" }
+        sink = ltn12.sink.table(t),
+        headers = { ["User-Agent"]= "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" }
       }
 -- Host: wifi.ais.co.th
 -- User-Agent: curl/7.47.0
--- Accept: */*
+-- Accept: */*t)
 
     if b then
+      local body = table.concat(t)
 -- https://wifi.ais.co.th/WebResource.axd?d=IMkPF5uiIUCr_cpszKEso1&amp;t=635307591758913288
-      print ("LUA BODY " .. " len " .. string.len(b) .. " type " .. type(b))
-      local matched = string.match(b, 'https://wifi.ais.co.th/WebResource.axd.*t=%d+')
+      print ("LUA BODY " .. " len " .. string.len(body) .. " type " .. type(body))
+      local matched = string.match(body, '/WebResource.axd.*t=%d+')
       print ("LUA match " .. " type " .. type(matched) )
-      print ("LUA STATUS " .. c .. " type " .. type(c) .. sl)
+      if matched then
+        print (matched)
+        local bodyok, status, headers, status_line = http.request {
+            url = urlr .. matched,
+            headers = { ["User-Agent"]= "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0" }
+          }
+        print ("LUA AUTH STATUS " .. status_line )
+        for k,v in pairs(headers) do
+         print (k, v)
+        end
+      end
+      print ("LUA STATUS " .. sl )
       print ("LUA h " .. " type " .. type(h) )
       for k,v in pairs(h) do
        print (k, v)
